4e53e8e2906984380b2faa161debf861462e56b6
Rafael Bitencourt <rafaelbitencourrt@gmail.com>
2023-05-20 11:41:00 -0300

    visitando uma pagina na web

---
+++ cypress/e2e/testes/Test1.spec.js
@@ -0,0 +1,15 @@
+describe('My First Test Suite', function(){
+
+    it('My First Test case', function(){
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    })
+
+    it('My second test case', function(){
+
+
+        //test step
+
+    })
+
+
+})
