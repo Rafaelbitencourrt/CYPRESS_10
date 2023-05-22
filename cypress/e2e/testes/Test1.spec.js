3fe08e38e901107f5a774b1b7fc9a2adb6b9f5e3
Rafael Bitencourt <rafaelbitencourrt@gmail.com>
2023-05-22 15:54:02 -0300

    adicionado teste

--- cypress/e2e/testes/Test1.spec.js
+++ cypress/e2e/testes/Test1.spec.js
@@ -1,15 +1,12 @@
 describe('My First Test Suite', function(){
+    
 
     it('My First Test case', function(){
+
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.wait(5000)
+        cy.get('.search-keyword').type('brocoll')
+        cy.get('.products').contains('Brocolli').should('be.visible')
     })
-
-    it('My second test case', function(){
-
-
-        //test step
-
-    })
-
 
 })
